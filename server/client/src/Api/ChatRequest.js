3e6e8657ab46c7a678118d0f885d8db9d7ca9458
Deven Rathod <devenrathod2001@gmail.com>
2022-07-25 09:47:28 +0530

    Changed Port 8080 to 5000

--- server/client/src/Api/ChatRequest.js
+++ server/client/src/Api/ChatRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL:"http://localhost:8080"});
+const API = axios.create({baseURL:"http://localhost:5000"});
 
 
 export const userChats = (id) => API.get(`/chat/${id}`);
--- server/client/src/Api/PostRequest.js
+++ server/client/src/Api/PostRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL:"http://localhost:8080"})
+const API = axios.create({baseURL:"http://localhost:5000"})
 
 export const getTimelinePosts = (id) => API.get(`/post/${id}/timeline`)
 
